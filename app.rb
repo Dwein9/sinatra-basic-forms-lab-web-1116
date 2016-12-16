eab06fb125366074d68dc4c98c3d585d9d67dde8
David Weinstein <txea@Davids-MacBook-Air.local>
2016-12-16 15:23:10 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,22 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/'  do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/pups' do
+    # erb :create_puppy
+    @pup = Puppy.new(params["name"], params["breed"], params["age"])
+    erb :display_puppy
+  end
+  #
+  #   redirect ?
+  #
+  #
+  # end
 end
